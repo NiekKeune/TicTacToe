110d26e4a7d051308fe0958da64319091d8d874d
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2018-01-16 11:53:38 +0100

    Final Functionality
    Laatste functionaliteit toegevoegd, code is alleen een rommeltje en het ontbreekt ook nog goede naamgeving en commentaar.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(10, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(50, 50);
@@ -51,6 +52,7 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(66, 50);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(50, 50);
@@ -60,6 +62,7 @@
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.Location = new System.Drawing.Point(122, 50);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(50, 50);
@@ -69,6 +72,7 @@
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(10, 102);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(50, 50);
@@ -78,6 +82,7 @@
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.Location = new System.Drawing.Point(66, 102);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(50, 50);
@@ -87,6 +92,7 @@
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.Location = new System.Drawing.Point(122, 102);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(50, 50);
@@ -96,6 +102,7 @@
             // 
             // button7
             // 
+            this.button7.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.Location = new System.Drawing.Point(10, 158);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(50, 50);
@@ -105,6 +112,7 @@
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.Location = new System.Drawing.Point(66, 158);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(50, 50);
@@ -114,6 +122,7 @@
             // 
             // button9
             // 
+            this.button9.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button9.Location = new System.Drawing.Point(122, 158);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(50, 50);
@@ -128,6 +137,7 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(160, 20);
             this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -25,6 +25,7 @@ namespace TicTacToe
         bool button8Status = true;
         bool button9Status = true;
 
+        bool gameSession = true;
 
         public Form1()
         {
@@ -43,171 +44,221 @@ namespace TicTacToe
 
         }
 
-        private void SettingButtons()
+        private void CheckingFinish()
         {
+            if (button1.Text == "x" && button2.Text == "x" && button3.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button4.Text == "x" && button5.Text == "x" && button6.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button7.Text == "x" && button8.Text == "x" && button9.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button1.Text == "x" && button4.Text == "x" && button7.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button2.Text == "x" && button5.Text == "x" && button8.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button3.Text == "x" && button6.Text == "x" && button9.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button1.Text == "x" && button5.Text == "x" && button9.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
+            else if (button3.Text == "x" && button5.Text == "x" && button7.Text == "x")
+                    { this.textBox1.Text = "Player 1 won."; gameSession = false; }
 
+            else if (button1.Text == "o" && button2.Text == "o" && button3.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button4.Text == "o" && button5.Text == "o" && button6.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button7.Text == "o" && button8.Text == "o" && button9.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button1.Text == "o" && button4.Text == "o" && button7.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button2.Text == "o" && button5.Text == "o" && button8.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button3.Text == "o" && button6.Text == "o" && button9.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button1.Text == "o" && button5.Text == "o" && button9.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button3.Text == "o" && button5.Text == "o" && button7.Text == "o")
+                    { this.textBox1.Text = "Player 2 won."; gameSession = false; }
+            else if (button1Status == false && button2Status == false && button3Status == false && button4Status == false 
+                    && button5Status == false && button6Status == false && button7Status == false && button8Status == false 
+                    && button9Status == false && gameSession == true)
+                        { this.textBox1.Text = "The game is a draw"; }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button1Status == true)
+            if (player1 == true && button1Status == true && gameSession == true)
             {
                 button1.Text = "x";
                 player1 = false;
                 player2 = true;
                 button1Status = false;
             }
-            else if (player2 == true && button1Status == true)
+            else if (player2 == true && button1Status == true && gameSession == true)
             {
                 button1.Text = "o";
                 player2 = false;
                 player1 = true;
                 button1Status = false;
             }
+            CheckingFinish();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button2Status == true)
+            if (player1 == true && button2Status == true && gameSession == true)
             {
                 button2.Text = "x";
                 player1 = false;
                 player2 = true;
                 button2Status = false;
             }
-            else if (player2 == true && button2Status == true)
+            else if (player2 == true && button2Status == true && gameSession == true)
             {
                 button2.Text = "o";
                 player2 = false;
                 player1 = true;
                 button2Status = false;
             }
+            CheckingFinish();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button3Status == true)
+            if (player1 == true && button3Status == true && gameSession == true)
             {
                 button3.Text = "x";
                 player1 = false;
                 player2 = true;
                 button3Status = false;
             }
-            else if (player2 == true && button3Status == true)
+            else if (player2 == true && button3Status == true && gameSession == true)
             {
                 button3.Text = "o";
                 player2 = false;
                 player1 = true;
                 button3Status = false;
             }
+            CheckingFinish();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button4Status == true)
+            if (player1 == true && button4Status == true && gameSession == true)
             {
                 button4.Text = "x";
                 player1 = false;
                 player2 = true;
                 button4Status = false;
             }
-            else if (player2 == true && button4Status == true)
+            else if (player2 == true && button4Status == true && gameSession == true)
             {
                 button4.Text = "o";
                 player2 = false;
                 player1 = true;
                 button4Status = false;
             }
+            CheckingFinish();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button5Status == true)
+            if (player1 == true && button5Status == true && gameSession == true)
             {
                 button5.Text = "x";
                 player1 = false;
                 player2 = true;
                 button5Status = false;
             }
-            else if (player2 == true && button5Status == true)
+            else if (player2 == true && button5Status == true && gameSession == true)
             {
                 button5.Text = "o";
                 player2 = false;
                 player1 = true;
                 button5Status = false;
             }
+            CheckingFinish();
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button6Status == true)
+            if (player1 == true && button6Status == true && gameSession == true)
             {
                 button6.Text = "x";
                 player1 = false;
                 player2 = true;
                 button6Status = false;
             }
-            else if (player2 == true && button6Status == true)
+            else if (player2 == true && button6Status == true && gameSession == true)
             {
                 button6.Text = "o";
                 player2 = false;
                 player1 = true;
                 button6Status = false;
             }
+            CheckingFinish();
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button7Status == true)
+            if (player1 == true && button7Status == true && gameSession == true)
             {
                 button7.Text = "x";
                 player1 = false;
                 player2 = true;
                 button7Status = false;
             }
-            else if (player2 == true && button7Status == true)
+            else if (player2 == true && button7Status == true && gameSession == true)
             {
                 button7.Text = "o";
                 player2 = false;
                 player1 = true;
                 button7Status = false;
             }
+            CheckingFinish();
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button8Status == true)
+            if (player1 == true && button8Status == true && gameSession == true)
             {
                 button8.Text = "x";
                 player1 = false;
                 player2 = true;
                 button8Status = false;
             }
-            else if (player2 == true && button8Status == true)
+            else if (player2 == true && button8Status == true && gameSession == true)
             {
                 button8.Text = "o";
                 player2 = false;
                 player1 = true;
                 button8Status = false;
             }
+            CheckingFinish();
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (player1 == true && button9Status == true)
+            if (player1 == true && button9Status == true && gameSession == true)
             {
                 button9.Text = "x";
                 player1 = false;
                 player2 = true;
                 button9Status = false;
             }
-            else if (player2 == true && button9Status == true)
+            else if (player2 == true && button9Status == true && gameSession == true)
             {
                 button9.Text = "o";
                 player2 = false;
                 player1 = true;
                 button9Status = false;
             }
+            CheckingFinish();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
